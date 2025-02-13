3b5e5298558e0aab3e30f59301443bff67f73abb
Aidar Tokonov <aydarmaratov17@gmail.com>
2025-02-13 10:36:51 +0600

    Added Comment model

--- Api/Models/Comment.cs
+++ Api/Models/Comment.cs
@@ -2,5 +2,17 @@
 
 public class Comment
 {
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Content { get; set; } = string.Empty;
+
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    
+    public int? StockId { get; set; }
+    
+    public Stock? Stock { get; set; }
+    
     
 }
