9624834883ef6cd97eee0e86524bee3da8897957
Aidar Tokonov <aydarmaratov17@gmail.com>
2025-02-15 19:06:01 +0600

    Added Ef core, dbcontext and initial migration

--- Api/Data/ApplicationDbContext.cs
+++ Api/Data/ApplicationDbContext.cs
@@ -1,6 +1,15 @@
+using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Api.Data;
 
-public class ApplicationDbContext
+public class ApplicationDbContext : DbContext
 {
-    
+    public ApplicationDbContext(DbContextOptions options) : base(options) 
+    {
+           
+    }
+
+    public DbSet<Stock> Stocks {get; set;}
+    public DbSet<Comment> Comments {get; set;}
 }
