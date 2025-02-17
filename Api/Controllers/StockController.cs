1b9d2092c04492016c5bfd3a3e27ddcb20b94d5f
Aidar Tokonov <aydarmaratov17@gmail.com>
2025-02-17 10:56:57 +0600

    Added get actions to stockcontroller

--- Api/Controllers/StockController.cs
+++ Api/Controllers/StockController.cs
@@ -1,3 +1,7 @@
+using Api.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 namespace Api.Controllers;
 
 [Route("api/stock")]
@@ -9,5 +13,26 @@ public class StockController : ControllerBase
     public StockController(ApplicationDbContext context) 
     {
         _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var stocks = await _context.Stocks.ToListAsync();
+
+        return Ok(stocks);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById([FromRoute] int id) 
+    {
+        var stock = await _context.Stocks.FindAsync(id);
+
+        if (stock == null) 
+        {
+            return NotFound();
+        }
+
+        return Ok(stock);
     }
 }
